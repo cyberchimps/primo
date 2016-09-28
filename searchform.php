e93207f787bb5394b77066ca54d864e45ec58a6c
Prasada <prasada.pujari@hbwsl.com>
2016-09-28 16:21:05 +0530

    Small bugfixes

--- searchform.php
+++ searchform.php
@@ -18,7 +18,7 @@ $search_text = esc_attr( cyberchimps_get_option( 'searchbar_text' ) );
 if( !$search_text )
 	$search_text = esc_attr( 'Search &hellip;', 'primo' );
 ?>
-<form method="get"  id="searchform" class="navbar-search pull-right" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<form method="get"  id="searchform" class="navbar-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<input type="text" class="search-query input-medium" name="s" placeholder="<?php echo $search_text; ?>" />
 </form>
 <div class="clear"></div>
